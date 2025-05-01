75086c8ad4fd0761f93c56ed989a399324921ecd
xsieux292 <xsiseux292@gmail.com>
2025-05-01 16:51:03 +0700

    Update EvenltListener

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-
+const menu_icon = document.querySelector('#menu-icon')
+const nav_bar = document.querySelector('nav')
+menu_icon.addEventListener('click',toggleMenu)
 function toggleMenu() {
-  const menu_icon = document.querySelector('#menu-icon');
-  const nav_bar = document.querySelector('nav');
-    // เปลี่ยนสถานะของ menu-icon ด้วยการเพิ่มหรือลบ class "active"
-    menu_icon.classList.toggle('active');
-    nav_bar.classList.toggle('active');
-  }
+  // เปลี่ยนสถานะของ menu-icon ด้วยการเพิ่มหรือลบ class "active"
+  menu_icon.classList.toggle('active')
+  nav_bar.classList.toggle('active')
+}
